b21667565e34054c705f0803c4eb25055da9221e
Jersk41 <japarssidik820@gmail.com>
2024-06-05 21:10:14 +0700

    create Todos and TodoItem component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
 import React, { useState } from 'react'
+import Todos from './components/Todos'
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      title: 'Finish Progate React Course',
-      completed: false,
-    },
-    {
-      id: 2,
-      title: 'Have lunch with Guru Domba',
-      completed: false,
-    },
-    {
-      id: 3,
-      title: 'Study React with Ninja Ken',
-      completed: false,
-    },
-  ])
+    const [todos, setTodos] = useState([
+        {
+            id: 1,
+            title: 'Finish Progate React Course',
+            completed: false,
+        },
+        {
+            id: 2,
+            title: 'Have lunch with Guru Domba',
+            completed: false,
+        },
+        {
+            id: 3,
+            title: 'Study React with Ninja Ken',
+            completed: false,
+        },
+    ])
 
-  console.log(todos)
+    console.log(todos)
 
-  return (
-    <div>
-      <h1>My Todo List</h1>
-      {/* Gunakan method map di sini */}
-      {todos.map((todo) => {
-        return <p key={todo.id}>{todo.title}</p>
-      })}
-    </div>
-  )
+    return (
+        <div>
+            <h1>My Todo List</h1>
+            <Todos todos={todos} />
+        </div>
+    )
 }
 
 export default App
